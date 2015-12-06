f5399f80005a7eecf41672d37155772e2c30fb6d
Richard Davey <rdavey@gmail.com>
2015-12-06 12:57:43 +0000

    More combo tests.

---
+++ src/keyboard/07-keyComboMaxDelay.js
@@ -0,0 +1,31 @@
+
+import AddKeyDown from 'input/keyboard/events/AddKeyDown.js';
+import KeyCombo from 'input/keyboard/combo/KeyCombo.js';
+import ProcessKeyCombo from 'input/keyboard/combo/ProcessKeyCombo.js';
+
+function checkCombo (event, combo) {
+
+    let result = ProcessKeyCombo(event, combo);
+
+    if (result)
+    {
+        console.log('COMBO MATCHED!');
+    }
+
+}
+
+//  37 = LEFT
+//  38 = UP
+//  39 = RIGHT
+//  40 = DOWN
+
+let combo = KeyCombo([ 38, 38, 38, 40, 40, 40, 37, 37, 37, 39, 39, 39 ]);
+
+//  They get 1 second to press the next key in the combo or it resets
+combo.maxKeyDelay = 1000;
+
+console.log(combo);
+
+AddKeyDown(window, e => checkCombo(e, combo));
+
+console.log('Keyboard ready');
