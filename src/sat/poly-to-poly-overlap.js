0dc39309592f8b73c7473404d3005d6ebd2dd7f8
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2016-05-10 14:05:06 -0300

    Added test for polygon to polygon overlap.

---
+++ src/sat/poly-to-poly-overlap.js
@@ -0,0 +1,35 @@
+import PolyToPoly from 'sat/overlap/PolygonToPolygon.js'
+import Vec2 from 'math/vector/vec2/Vec2.js'
+
+let offset = new Vec2(80, 150);
+
+const poly0 = [
+    new Vec2(offset.x + 0, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 0)
+];
+
+offset.set(0, 130);
+
+const poly1 = [
+    new Vec2(offset.x + 0, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 0)
+];
+
+offset.set(400, 400);
+
+const poly2 = [
+    new Vec2(offset.x + 0, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 100),
+    new Vec2(offset.x + 100, offset.y + 0),
+    new Vec2(offset.x + 0, offset.y + 0)
+];
+
+console.log('poly0 overlap poly1 ? should be true', PolyToPoly(poly0, poly1));
+console.log('poly1 overlap poly2 ? should be false', PolyToPoly(poly1, poly2));
