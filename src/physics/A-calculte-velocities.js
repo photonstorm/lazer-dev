5502eb0c91240b11f074115c9bb4c6c0b5041a11
Felipe Alfonso <f.alfonso.espinosa@gmail.com>
2016-05-26 19:28:55 -0400

    Added example for collision solving.

--- src/physics/A-calculte-velocities.js
+++ src/physics/A-calculte-velocities.js
@@ -2,9 +2,12 @@ import {
     RunSimulationFrame
 }
 from 'physics/arcade/PhysicsSystem.js'
+import {
+    RectangleCollider
+} from 'physics/arcade/Collider.js'
 import Body from 'physics/arcade/Body.js'
 import Vec2 from 'math/vector/vec2/Vec2.js'
-    // Rendering
+// Rendering
 import Canvas from 'canvas/Canvas.js';
 import AddToDOM from 'dom/AddToDOM.js';
 import BackgroundColor from 'canvas/BackgroundColor.js';
@@ -28,7 +31,7 @@ export default class App {
         BackgroundColor(canvas, 'rgb(0, 0, 20)');
         ctx = canvas.getContext('2d');
         for (let i = 0; i < bodies.length; ++i) {
-            bodies[i] = Body(canvas.width / 2, canvas.height / 2, 4, 4);
+            bodies[i] = new Body(canvas.width / 2, canvas.height / 2, new RectangleCollider(0, 0, 4, 4));
             bodies[i].acceleration.y = 0.4;
             bodies[i].velocity.x = getRandom(-5.0, 5.0);
             bodies[i].velocity.y = getRandom(-10.0, -1.0);
@@ -41,16 +44,23 @@ export default class App {
         ctx.fillStyle = '#fff';
 
         function begin() {
-            ctx.clearRect(0, 0, 512, 512);            
+            ctx.clearRect(0, 0, 512, 512);
         }
+
         function update() {
             RunSimulationFrame(loop.physicsStep);
         }
+
         function draw() {
             for (let i = 0; i < bodies.length; ++i) {
-                ctx.fillRect(bodies[i].position.x, bodies[i].position.y, 4, 4);
+                ctx.fillRect(
+                    bodies[i].position.x, 
+                    bodies[i].position.y, 
+                    bodies[i].collider.width, 
+                    bodies[i].collider.height
+                );
                 if (bodies[i].position.y > 512) {
-                    bodies[i].position.x = canvas.width/ 2;
+                    bodies[i].position.x = canvas.width / 2;
                     bodies[i].position.y = canvas.height / 2;
                     bodies[i].velocity.y = getRandom(-10.0, -1.0);
                 }
---
+++ src/physics/B-collision-solving.js
@@ -0,0 +1,123 @@
+import {
+    RunSimulationFrame
+} from 'physics/arcade/PhysicsSystem.js'
+import {
+    RectangleCollider
+} from 'physics/arcade/Collider.js';
+import {
+    Collide,
+    RunCollisionFrame,
+    EmitCollisionCallbacks
+} from 'physics/arcade/CollisionSystem.js'
+import Body from 'physics/arcade/Body.js'
+import Vec2 from 'math/vector/vec2/Vec2.js'
+// Rendering
+import Canvas from 'canvas/Canvas.js';
+import AddToDOM from 'dom/AddToDOM.js';
+import BackgroundColor from 'canvas/BackgroundColor.js';
+import MainLoop from 'system/MainLoop.js';
+
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function drawPoly(ctx, pos, verticesX, verticesY) {
+    if (verticesY.length > 2) {
+        ctx.beginPath();
+        ctx.moveTo(pos.x + verticesX[0], pos.y + verticesY[0]);
+        for (var i = 1; i < verticesY.length; ++i) {
+            ctx.lineTo(pos.x + verticesX[i], pos.y + verticesY[i]);
+        }
+        ctx.closePath();
+        ctx.stroke();
+        ctx.globalAlpha = 0.2;
+        ctx.fill();
+        ctx.globalAlpha = 1.0;
+        ctx.fillStyle = '#fff';
+        for (var i = 0; i < verticesY.length; ++i) {
+            ctx.fillRect(pos.x + verticesX[i] - 2, pos.y + verticesY[i] - 2, 4, 4);
+        }
+    }
+}
+export default class App {
+
+    constructor() {
+        let mouse = new Vec2(0, 0);
+        let canvas = null;
+        let ctx = null;
+        let bodyA = null;
+        let bodyB = null;
+
+        canvas = Canvas(512, 512);
+        AddToDOM(canvas, 'game');
+        BackgroundColor(canvas, 'rgb(0, 0, 20)');
+        ctx = canvas.getContext('2d');
+        canvas.addEventListener('mousemove', function (evt) {
+            mouse.x = evt.clientX - evt.target.offsetLeft;
+            mouse.y = evt.clientY - evt.target.offsetTop;
+        });
+        ctx.fillStyle = '#fff';
+
+        bodyA = new Body(256, 356, new RectangleCollider(-50, -50, 100, 100));
+        bodyB = new Body(256, 150, new RectangleCollider(-12, -30, 25, 60));
+
+        bodyA.acceleration.y = -0.1;
+        bodyB.bounce.y = 1;
+        bodyA.bounce.y = 1;
+        bodyB.acceleration.y = 0.15;
+
+
+        function begin() {
+            ctx.clearRect(0, 0, 512, 512);
+        }
+
+        function BodiesCollided() {
+            console.log('Collision detected');
+        }
+
+        function update() {
+            RunSimulationFrame(loop.physicsStep);
+            Collide(bodyA, bodyB, BodiesCollided);
+
+            // Run this after all collision request
+            // have been done.
+            RunCollisionFrame();
+            EmitCollisionCallbacks();
+
+            if (bodyA.position.y < 0) {
+                bodyA.position.x = canvas.width / 2;
+                bodyA.position.y = canvas.height;
+                bodyA.velocity.y = getRandom(-10.0, -1.0);
+            }
+            if (bodyB.position.y > 512) {
+                bodyB.position.x = canvas.width / 2;
+                bodyB.position.y = -bodyB.collider.height;
+                bodyB.velocity.y = getRandom(-10.0, -1.0);
+            }
+        }
+
+        function draw() {
+            /*ctx.fillRect(
+                bodyA.position.x,
+                bodyA.position.y,
+                bodyA.collider.width,
+                bodyA.collider.height
+            );
+            ctx.fillRect(
+                bodyB.position.x,
+                bodyB.position.y,
+                bodyB.collider.width,
+                bodyB.collider.height
+            );*/
+            drawPoly(ctx, bodyA.position, bodyA.collider.verticesX, bodyA.collider.verticesY);
+            drawPoly(ctx, bodyB.position, bodyB.collider.verticesX, bodyB.collider.verticesY);
+            ctx.fillText('fps: ' + loop.fps.toFixed(2), 16, 16);
+        }
+        let loop = new MainLoop(60);
+        loop.begin = (t => begin(t));
+        loop.update = (delta => update(delta));
+        loop.draw = (t => draw(t));
+        loop.start();
+    }
+}
+
+new App();
